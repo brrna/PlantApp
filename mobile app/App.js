e85fa300c40b398e2195869bb8b93c0e969a5d75
emresen20 <esen20@posta.pau.edu.tr>
2024-03-25 19:21:45 +0300

    start page added

--- mobile app/App.js
+++ mobile app/App.js
@@ -1,11 +1,21 @@
 import { SafeAreaView, Text } from "react-native";
 import Router from "./src/pages/Router";
 import { NavigationContainer } from "@react-navigation/native";
+import Start from "./src/pages/Start";
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [splash, setSplash] = useState(true);
+
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     setSplash(false);
+  //   }, 5000)
+  // })
+
   return(
     <NavigationContainer>
-      <Router />
+       {splash ? <Start setSplash={setSplash}/> : <Router/>}
     </NavigationContainer>
   )
 }
--- mobile app/src/pages/Start.js
+++ mobile app/src/pages/Start.js
@@ -1,20 +1,32 @@
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, StyleSheet, Text, View,Button } from "react-native";
 import { heightPercentageToDP as hp, widthPercentageToDP as wp } from "react-native-responsive-screen";
 import React from "react";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-function Start() {
+function Start({ setSplash}) {
+
+    // const handlePress = () => {
+    //     setSplash(false); // Handle pressi onpresin içine de koyup 
+    // };
+
+    
     return (
         <SafeAreaView style={styles.safecontainer}>
             <View style={styles.firstview}>
                 <Text>This is start</Text>
             </View>
             <View style={styles.secondview}>
-                <View style={{justifyContent:"flex-start",alignItems:"flex-start",marginLeft:hp(3)}}>
+                <View style={{ justifyContent: "flex-start", alignItems: "flex-start", marginLeft: hp(3) }}>
                     <Text>Scan your</Text>
                     <Text>plant and find</Text>
                     <Text>out what it is.</Text>
                 </View>
                 <View style={{}}>
+                    <Button
+                        title="Go to Details"
+                        onPress={()=>setSplash(false)}
+                    />
                     <Text>Scan your</Text>
                     <Text>plant and find</Text>
                     <Text>out what it is.</Text>
