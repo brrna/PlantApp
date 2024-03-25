10b1a839af015155ce92a2e71e68c84166812413
emresen20 <esen20@posta.pau.edu.tr>
2024-03-25 21:48:14 +0300

    welcome page designed

--- mobile app/src/pages/Start.js
+++ mobile app/src/pages/Start.js
@@ -1,40 +1,49 @@
-import { SafeAreaView, StyleSheet, Text, View,Button } from "react-native";
+import { SafeAreaView, StyleSheet, Text, View, Button, Image, TouchableOpacity } from "react-native";
 import { heightPercentageToDP as hp, widthPercentageToDP as wp } from "react-native-responsive-screen";
 import React from "react";
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-function Start({ setSplash}) {
+function Start({ setSplash }) {
 
     // const handlePress = () => {
-    //     setSplash(false); // Handle pressi onpresin içine de koyup 
+    //     setSplash(false); // Handle pressi onpresin içine de koyup
     // };
 
-    
+
     return (
         <SafeAreaView style={styles.safecontainer}>
             <View style={styles.firstview}>
-                <Text>This is start</Text>
+                <Image style={styles.leapimage} source={require("../images/pngFirstPage.png")} />
+                <View style={styles.welcomeinsadeview}>
+                    <View style={styles.welcomeview}>
+                        <Text style={styles.welcometext}>WELCOME !</Text>
+                    </View>
+                </View>
+
+
             </View>
             <View style={styles.secondview}>
-                <View style={{ justifyContent: "flex-start", alignItems: "flex-start", marginLeft: hp(3) }}>
-                    <Text>Scan your</Text>
-                    <Text>plant and find</Text>
-                    <Text>out what it is.</Text>
+                <View style={{ justifyContent: "flex-start", alignItems: "flex-start", marginLeft: hp(4) }}>
+                    <Text style={styles.texts}>Scan your</Text>
+                    <Text style={styles.texts}>plant and find</Text>
+                    <Text style={styles.texts}>out what it is.</Text>
                 </View>
-                <View style={{}}>
-                    <Button
-                        title="Go to Details"
-                        onPress={()=>setSplash(false)}
-                    />
-                    <Text>Scan your</Text>
-                    <Text>plant and find</Text>
-                    <Text>out what it is.</Text>
+                <View style={{ justifyContent: "flex-start", alignItems: "flex-start", marginLeft: hp(4), marginTop: hp(3) }}>
+
+                    <Text style={styles.texts}>A perfect app</Text>
+                    <Text style={styles.texts}>for plant</Text>
+                    <Text style={styles.texts}>lovers.</Text>
                 </View>
-
-
-
-
+                <View >
+                    <TouchableOpacity  onPress={() => setSplash(false)} style={styles.toucahableopacityget}>
+                        <Text style={styles.tocuhabletext}>Get Started</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={{justifyContent:"center",alignItems:"center",marginTop:hp(2)}}>
+                        <Text>By tapping next, you are agreeing to LeafLove </Text>
+                        <Text>Terms of Use &  Privacy Policy</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
         </SafeAreaView>
     )
@@ -44,17 +53,66 @@ export default Start;
 
 const styles = StyleSheet.create({
     safecontainer: {
-        flex: 1
+        flex: 1,
+        backgroundColor: "#D4EAF1"
     },
     firstview: {
         flex: 1,
-        backgroundColor: "red"
+        backgroundColor: "#D4EAF1"
 
     },
     secondview: {
         flex: 1,
-        backgroundColor: "blue"
 
+    },
+    leapimage: {
+        width: wp(35),
+        height: hp(35),
+        marginLeft: wp(24)
+    },
+    welcomeview: {
+        width: wp(58),
+        height: hp(8),
+        backgroundColor: "#A2DFA4",
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: -hp(1),
+        marginLeft: -wp(2)
+
+    },
+    welcomeinsadeview: {
+        width: wp(59),
+        height: hp(8),
+        backgroundColor: "black",
+        marginLeft: wp(38),
+        marginTop: hp(2),
+
+
+    },
+    welcometext: {
+        color: "#000000",
+        fontSize: hp(3.5),
+        fontWeight: "bold"
+
+    },
+    texts: {
+        fontSize: hp(2.75),
+        color: "#555F80"
+    },
+    toucahableopacityget: {
+        width: wp(80), 
+        backgroundColor: "red",
+         height: hp(6), alignSelf: "center", 
+         borderRadius: hp(5), 
+         justifyContent: "center", 
+         alignItems: "center",
+         marginTop:hp(5),
+         backgroundColor:"#26ECA5"
+
+    },
+    tocuhabletext:{
+        color:"white",
+        fontSize:hp(2.5)
     }
 
 })
