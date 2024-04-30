d0a3c7e664c53f954982852757c1b595424371fd
emresen20 <esen20@posta.pau.edu.tr>
2024-04-30 14:27:26 +0300

    Bottoom TAb Customize for now - working on it

--- mobile app/src/pages/Router.js
+++ mobile app/src/pages/Router.js
@@ -24,8 +24,23 @@ function Router() {
 
     return(
             <Tab.Navigator
+
+            // screenOptions={{
+            //     tabBarShowLabel: false,
+            //     tabBarStyle: {
+            //         position: 'absolute',
+            //         bottom: 0,
+            //         left: 0,
+            //         right: 0,
+            //         elevation: 0,
+            //         backgroundColor: '#ffffff',
+            //         borderRadius: 15,
+            //         height: 75,
+            //     }
+            // }}
                 screenOptions={({ route }) => 
                 ({
+                    tabBarShowLabel : false,
                     tabBarIcon: ({ focused, color, size }) => {
                         let iconName;
 
---
+++ mobile app/src/pages/WelcomePage.js
@@ -0,0 +1,20 @@
+import { SafeAreaView, StyleSheet } from "react-native";
+
+
+
+function Welcome(){
+    return(
+        <SafeAreaView style={styles.container}
+        >
+
+        </SafeAreaView>
+    )
+}
+
+export default Welcome;
+
+const styles =StyleSheet.create({
+    container:{
+        flex:1
+    }
+})
