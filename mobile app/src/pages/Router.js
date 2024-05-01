f22ea40b56c18342bced2ffaa87a8316c5da4e4e
emresen20 <esen20@posta.pau.edu.tr>
2024-05-01 14:10:42 +0300

    Tabbar uptaded final state

--- mobile app/src/pages/Router.js
+++ mobile app/src/pages/Router.js
@@ -1,5 +1,5 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { SafeAreaView } from "react-native";
+import { SafeAreaView, StyleSheet, TouchableOpacity, View } from "react-native";
 import { heightPercentageToDP as hp, widthPercentageToDP as wp } from "react-native-responsive-screen";
 import Home from "./Home";
 import Camera from "./Camera";
@@ -11,77 +11,95 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 function Router() {
 
+
     const Tab = createBottomTabNavigator();
     const Stack = createNativeStackNavigator();
 
-    const StartStack =()=>{
-        return(
+    const CustomTabBarButton = ({ children, onPress }) => (
+        <TouchableOpacity
+            style={{
+                top: -20
+
+            }}>
+
+        </TouchableOpacity>
+    )
+
+    const StartStack = () => {
+        return (
             <Stack.Navigator>
                 <Stack.Screen name="Start" component={Start} />
             </Stack.Navigator>
         )
     }
 
-    return(
-            <Tab.Navigator
+    return (
+        <Tab.Navigator
 
-            // screenOptions={{
-            //     tabBarShowLabel: false,
-            //     tabBarStyle: {
-            //         position: 'absolute',
-            //         bottom: 0,
-            //         left: 0,
-            //         right: 0,
-            //         elevation: 0,
-            //         backgroundColor: '#ffffff',
-            //         borderRadius: 15,
-            //         height: 75,
-            //     }
-            // }}
-                screenOptions={({ route }) => 
-                ({
-                    tabBarShowLabel : false,
-                    tabBarIcon: ({ focused, color, size }) => {
-                        let iconName;
+            screenOptions={{
+                tabBarShowLabel: false,
+                tabBarStyle: {
+                    position: 'absolute',
+                    bottom: hp(0),
+                    left: hp(0),
+                    right: hp(0),
+                    elevation: 0,
+                    backgroundColor: '#D4EBD2',
+                    height: hp(6.5),
+                }
+            }}
 
-                        if (route.name === 'Home') {
-                            iconName = focused ? 'home' : 'home-outline';
-                        } else if (route.name === 'Camera') {
-                            iconName = focused ? 'camera' : 'camera-outline';
-                        } else if (route.name === 'Profile') {
-                            iconName = focused ? 'settings' : 'settings-outline';
-                        }
-                        else if (route.name === 'Start') {
-                            iconName = focused ? 'settings' : 'settings-outline';
-                        
-                        }
-                        return <Icon name={iconName} size={size} color={color} />;
-                    },
-                    headerShown: false,
-                    tabBarShowLabel:false,
-                    tabBarActiveTintColor: "black",
-                    tabBarInactiveTintColor: "blue",
-                    tabBarStyle: {
-                        backgroundColor: "white",
-                        height: hp(9),
-                    },
-                    tabBarLabelStyle: {
-                        fontSize: hp(4),
-                    },
-                    tabBarIconStyle: {
-                        marginBottom: route.name === "Camera" ? hp(5) : 0, // Kamera için alt boşluk
-                        borderRadius: route.name === "Camera" ? 999 : 0, // Sadece Camera için yuvarlak köşe ekleyin
-                        borderColor: "black", // Kenar rengi,
-                        backgroundColor:"red",
-                        //borderWidth: route.name === "Camera" ? 1 : 0, // Kamera için kenar kalınlığı
-                    }
-                })
-            }>
-                <Tab.Screen name="Home" component={Home} />
-                <Tab.Screen name="Camera" component={Camera} />
-                <Tab.Screen name="Profile" component={Profile} />
-            </Tab.Navigator>
+        >
+            <Tab.Screen name="Home" component={Home}
+                options={{
+                    tabBarIcon: ({ focused }) => (
+                        <Icon name={focused ? 'home-outline' : 'home'} size={hp(3.75)} color="#2C6817" />
+                    )
+
+                }} />
+            <Tab.Screen name="Camera" component={Camera}
+                options={{
+                    tabBarIcon: ({ focused }) => (
+                        <View style={styles.whiteview}>
+                            <View style={styles.greenview}>
+                                <Icon name={focused ? 'camera-outline' : 'camera'} size={hp(3.75)} color="#2C6817" />
+                            </View>
+                        </View>
+                    )
+                }} />
+            <Tab.Screen name="Profile" component={Profile}
+                options={{
+                    tabBarIcon: ({ focused }) => (
+                        <Icon name={focused ? 'person-circle-outline' : 'person-circle'} size={hp(4)} color="#2C6817" />
+                    )
+
+                }}
+            />
+        </Tab.Navigator>
     )
 }
+
+const styles = StyleSheet.create({
+    whiteview: {
+        borderRadius: 999,
+         width: wp(16.5), 
+         height: hp(8), 
+         backgroundColor: "white",
+         marginBottom: 40
+
+
+    },
+    greenview:{
+        width: wp(14), 
+        height: hp(7), 
+        borderRadius: 9999, 
+        backgroundColor: "#D4EBD2", 
+        justifyContent: "center", 
+        alignItems: "center", 
+        alignSelf: "center"
+
+    }
+
+})
 
 export default Router;
