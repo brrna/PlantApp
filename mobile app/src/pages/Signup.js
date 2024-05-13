fcfcd9f1c7bb2f70ad20dfed4a91d4f3451fed42
emresen20 <esen20@posta.pau.edu.tr>
2024-05-13 16:19:34 +0300

    sign up page finshed

--- mobile app/src/pages/Signup.js
+++ mobile app/src/pages/Signup.js
@@ -1,9 +1,10 @@
-import { Image, SafeAreaView, StyleSheet, Text, View, TouchableOpacity} from "react-native";
+import { Image, SafeAreaView, StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import MyHeader from "../component/myHeader/MyHeader";
 import Welcome from "../component/myWelcome/Welcome";
 import { heightPercentageToDP as hp, widthPercentageToDP as wp } from "react-native-responsive-screen";
 import MyTextinput from "../component/myTextInput/MyTextinput";
 import { useState } from "react";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 function Signup({ navigation }) {
     const [email, setEmail] = useState("");
@@ -12,33 +13,49 @@ function Signup({ navigation }) {
     const [confirmpassword, setConfirmpassword] = useState("");
     return (
 
-        <SafeAreaView style={styles.container}>
-            <MyHeader />
-            <Welcome text={"Welcome back!"} />
-            <View style={styles.textviewstyle}>
-                <Text style={styles.textstyle}> Explore the wonders of nature</Text>
-                <Text style={styles.textstyle}> through our botanical gateway.</Text>
-            </View>
-            <Image style={styles.imagestyle} source={require("../assests/images/plantBackground.png")} />
-            <View style={styles.textinpucontainer}>
-                <MyTextinput value={nickname} setValue={setNickname} placeholder={"Enter your nickname"} />
-                <MyTextinput value={email} setValue={setEmail} placeholder={"Enter your email"} style={{ marginTop: hp(2) }} />
-                <View style={styles.eyetextinput}>
-                    <MyTextinput value={password} setValue={setPassword} placeholder={"Enter your password"}/>
-                    <TouchableOpacity>
-                        <Image style={styles.eyeimage} source={require("../assests/images/eye.png")}/>
-                    </TouchableOpacity>
+        <KeyboardAwareScrollView enableOnAndroid={true}
+            resetScrollToCoords={{ x: 0, y: 0 }}
+            contentContainerStyle={{ flexGrow: 1 }}>
+            <SafeAreaView style={styles.container}>
+                <MyHeader />
+                <Welcome text={"Welcome back!"} />
+                <View style={styles.textviewstyle}>
+                    <Text style={styles.textstyle}> Explore the wonders of nature</Text>
+                    <Text style={styles.textstyle}> through our botanical gateway.</Text>
                 </View>
-                <View style={styles.eyetextinput}>
-                    <MyTextinput value={confirmpassword} setValue={setConfirmpassword} placeholder={"Confirm your oassword"}/>
-                    <TouchableOpacity>
-                        <Image style={styles.eyeimage} source={require("../assests/images/eye.png")}/>
-                    </TouchableOpacity>
+                <Image style={styles.imagestyle} source={require("../assests/images/plantBackground.png")} />
+                <View style={styles.textinpucontainer}>
+                    <MyTextinput value={nickname} setValue={setNickname} placeholder={"Enter your nickname"} />
+                    <MyTextinput value={email} setValue={setEmail} placeholder={"Enter your email"} style={{ marginTop: hp(2) }} />
+                    <View style={styles.eyetextinput}>
+                        <MyTextinput value={password} setValue={setPassword} placeholder={"Enter your password"} />
+                        <TouchableOpacity>
+                            <Image style={styles.eyeimage} source={require("../assests/images/eye.png")} />
+                        </TouchableOpacity>
+                    </View>
+                    <View style={styles.eyetextinput}>
+                        <MyTextinput value={confirmpassword} setValue={setConfirmpassword} placeholder={"Confirm your oassword"} />
+                        <TouchableOpacity>
+                            <Image style={styles.eyeimage} source={require("../assests/images/eye.png")} />
+                        </TouchableOpacity>
+                    </View>
+                    <View style={styles.signuptextview}>
+                        <TouchableOpacity>
+                            <Text style={styles.signuptext}> Sign up </Text>
+                        </TouchableOpacity>
+                        <View style={styles.viewrows}>
+                            <Text style={styles.textaccount}>Don't have an account?</Text>
+                            <TouchableOpacity>
+                                <Text style={styles.siginupblue} >Sign Up</Text>
+                            </TouchableOpacity>
+
+                        </View>
+                    </View>
+
                 </View>
+            </SafeAreaView>
+        </KeyboardAwareScrollView>
 
-            </View>
-
-        </SafeAreaView>
     )
 }
 
@@ -71,18 +88,39 @@ const styles = StyleSheet.create({
         marginTop: hp(58),
 
     },
-    eyetextinput:{
-        flexDirection:"row",
-        alignItems:"center",
-        position:"relative",
-        marginTop:hp(2)
+    eyetextinput: {
+        flexDirection: "row",
+        alignItems: "center",
+        position: "relative",
+        marginTop: hp(2)
     },
     eyeimage: {
         position: "absolute",
         right: wp(2),
         width: wp(4),
         height: hp(2),
-        bottom:-7
+        bottom: -7
     },
+    signuptextview: {
+        alignItems: "center",
+        marginTop: hp(2)
+    },
+    signuptext: {
+        color: "#1250AD",
+        fontWeight: "bold",
+        fontSize: hp(3)
+    },
+    viewrows: {
+        flexDirection: "row",
+        marginTop: hp(1),
+        gap: wp(1)
+    },
+    textaccount: {
+        fontWeight: "bold"
+    },
+    siginupblue: {
+        color: "#1250AD",
+        fontWeight: "bold"
+    }
 })
 export default Signup;
