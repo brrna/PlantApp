2fa7297b7806cc7e782a6072677a3c06b7991311
brrna <bern.varol@gmail.com>
2024-05-24 19:44:43 +0300

    favoriler fixed

--- mobile app/src/pages/Home.js
+++ mobile app/src/pages/Home.js
@@ -19,8 +19,7 @@ function Home() {
                 img={{ uri: item.Image }}
                 name={item.Name}
                 description={item.Description}
-                plantsId={item.Id}
-                roseId={item.Id} />
+                id={item.Id} />
         )
     }
 
